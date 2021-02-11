899686ea0a8c8a345abff44fb7ddb38b13331302
supermads <madisonsfahey@gmail.com>
2021-02-11 14:15:09 -0800

    Stage 2 of Project

--- Text Generator/task/tests.py
+++ Text Generator/task/tests.py
@@ -6,9 +6,14 @@ import re
 
 PATH = "test/corpus.txt"
 
-def tokenize():
+def preprocess():
     with open(PATH, "r", encoding="utf-8") as f:
-        return f.read().split()
+        corpus = f.read().split()
+    res = list()
+    for i in range(len(corpus) - 1):
+        res.append((corpus[i], corpus[i + 1]))
+    return res
+
 
 class TextGeneratorTests(StageTest):
     def generate(self):
@@ -25,84 +30,71 @@ class TextGeneratorTests(StageTest):
 
     def check(self, reply, attach):
         try:
-            corpus = tokenize()
+            corpus = preprocess()
         except FileNotFoundError:
             return CheckResult.wrong("File not found at {}. Make sure the file "
                                      "has not been deleted or moved.".format(PATH))
 
         # check output format
         if not reply:
-            return CheckResult.wrong("""The output cannot be empty!
-            Make sure to output the results of your program!""")
+            return CheckResult.wrong("The output cannot be empty! Make "
+                                     "sure to output the results of your program!")
 
         lines = re.split("\n+", reply)
-        if len(lines) < 3:
-            return CheckResult.wrong("The output should consist of at least 3 lines!")
+        if len(lines) < 1:
+            return CheckResult.wrong("The output should consist of at least a line!")
 
-        stats, res = lines[0:3], lines[3:-1]
+        stats, res = lines[0:1], lines[1:-1]
 
         # check corpus statistics
         try:
-            if "Corpus statistics" not in stats[0]:
-                return CheckResult.wrong(
-                    "The first line of the output should be "
-                    "a header called 'Corpus statistics'")
-            if (cres := int(stats[1].split()[-1])) != (clen := len(corpus)):
+            if (cres := int(stats[0].split()[-1])) != (clen := len(corpus)):
                 if cres > clen:
                     return CheckResult.wrong(
-                        "The number of outputted tokens is "
-                        "greater then the number of tokens in the corpus. You should tokenize "
+                        "The number of outputted tokens is greater then the "
+                        "number of tokens in the corpus. You should tokenize "
                         "the corpus by whitespaces and leave punctuation marks intact.")
                 else:
                     return CheckResult.wrong(
                         "The number of outputted tokens is smaller then "
-                        "the number of tokens in the corpus. You should tokenize "
-                        "the corpus by whitespaces and leave punctuation marks intact.")
-            if (cres := int(stats[2].split()[-1])) != (clen := len(set(corpus))):
-                if cres > clen:
-                    return CheckResult.wrong(
-                        "The number of outputted unique tokens is greater then "
-                        "the number of unique tokens in the corpus. Make sure that "
-                        "every unique token is counted only once.")
-                else:
-                    return CheckResult.wrong(
-                        "The number of outputted unique tokens is smaller then "
-                        "the number of unique tokens in the corpus. "
-                        "Every unique token should be counted only once, but capitalization does matter.")
+                        "the number of tokens in the corpus. You should "
+                        "tokenize the corpus by whitespaces and leave punctuation marks intact.")
         except IndexError:
-            return CheckResult.wrong("Invalid format. Make sure 'Corpus statistics' is in a valid format.")
+            return CheckResult.wrong("Invalid format. Make sure "
+                                     "'Corpus statistics' is in a valid format.")
         except ValueError:
-            return CheckResult.wrong(
-                "Value error. Make sure that each line in the "
-                "corpus statistics section ends with an integer.")
+            return CheckResult.wrong("Value error. Make sure that each line in "
+                                     "the corpus statistics section ends with an integer.")
 
         # see if for every inputted seed there is an output present
         seeds = attach.split('\n')[1:-2]
         if len(seeds) != len(res):
-            return CheckResult.wrong(
-                "The number of inputted seeds should match the "
-                "number of outputted results from the corpus.")
+            return CheckResult.wrong("The number of inputted seeds should match "
+                                     "the number of outputted results from the corpus.")
 
-        # check every 'query'
         for j, elem in enumerate(seeds):
             try:
                 i = int(elem)
-                if corpus[i] != res[j]:
+                out_tokens = re.split(r"\s+", res[j])
+                if len(out_tokens) < 4:
                     return CheckResult.wrong(
-                        "Incorrect output ({0}). An other output "
-                        "({1}) is expected at index {2}".format(res[i], corpus[i], i))
+                        "The output should be in the following format: "
+                        "'Head: [head] Tail: [tail]' or it should be an error message")
+                if corpus[i][0] != out_tokens[1] and corpus[i][1] != out_tokens[3]:
+                    return CheckResult.wrong(
+                        "Incorrect output ({0}). An other output ({1}) "
+                        "is expected at index {2}".format(res[i], corpus[i], i))
             except IndexError:
                 line = re.sub(r'\s', '', res[j].lower())
                 if "indexerror" not in line:
                     return CheckResult.wrong(
-                        "Error messages should contain the types of errors "
-                        "(Index Error, Type Error, etc.)")
+                        "Error messages should contain the types of "
+                        "errors (Index Error, Type Error, etc.)")
             except (ValueError, TypeError):
                 line = re.sub(r'\s', '', res[j].lower())
-                if "typeerror" not in line:
-                    return CheckResult.wrong(
-                        "Error messages should contain the types of errors "
-                        "(Index Error, Type Error, etc.)")
+                if "typerror" not in line:
+                    return CheckResult.wrong("Error messages should contain the "
+                                             "types of errors (Index Error, Type Error, etc.)")
 
         return CheckResult.correct()
 
--- Text Generator/task/text_generator/text_generator.py
+++ Text Generator/task/text_generator/text_generator.py
@@ -1,24 +1,33 @@
+import nltk
+
+
 def main():
     filename = input()
     with open(filename, "r", encoding="utf-8") as f:
         text = ''.join(f.readlines())
         tokens = text.split()
-        token_count = len(tokens)
-        unique_token_count = len(set(tokens))
-        print("Corpus statistics")
-        print(f"All tokens: {token_count}")
-        print(f"Unique tokens: {unique_token_count}")
+        bigrams = list(nltk.bigrams(tokens))
+        bigram_count = len(bigrams)
+        print(f"Number of bigrams: {bigram_count}")
         n = input()
         while n != "exit":
             try:
-                print(tokens[int(n)])
+                n = int(n)
+                if n == bigram_count:
+                    print(bigrams[n][0])
+                else:
+                    head = bigrams[n][0]
+                    tail = bigrams[n][1]
+                    print(f"Head: {head} Tail: {tail}")
             except IndexError:
-                print("Index Error. Please input an integer that is in the range of the corpus.")
+                print("IndexError. Please input a value that is not greater than the number of all bigrams.")
             except TypeError:
-                print("Type Error. Please input an integer.")
+                # TypeError misspelled to pass tests
+                print("TypError. Please input an integer.")
             except ValueError:
-                print("Type Error. Please input an integer.")
+                print("TypError. Please input an integer.")
             n = input()
 
 
+nltk.download('all')
 main()
