0c4bda5ada174bf0084fd137c809bc93cfee4903
supermads <madisonsfahey@gmail.com>
2021-03-03 15:45:55 -0800

    Corrected Stage 6 of Project

--- Text Generator/task/text_generator/text_generator.py
+++ Text Generator/task/text_generator/text_generator.py
@@ -8,10 +8,13 @@ def get_freq_dict():
     with open(filename, "r", encoding="utf-8") as f:
         text = ''.join(f.readlines())
         tokens = text.split()
+
         trigrams = list(nltk.trigrams(tokens))
         freq_dict = defaultdict(list)
+
         for trigram in trigrams:
             freq_dict[trigram[0:2]].append(trigram[2])
+
         for key, value in freq_dict.items():
             freq_dict[key] = Counter(value)
     return freq_dict
@@ -22,27 +25,33 @@ def generate_sentence(freq_dict):
     # Choose a random head from freq_dict to begin the sentence
     sentence = ""
     head = choice(list(freq_dict.keys()))
+
     # First word of the sentence should be capitalized and without punctuation marks
     while not head[0][0].isupper() or head[0][-1] in punc_marks:
         head = choice(list(freq_dict.keys()))
+
     sentence += f"{head[0]} {head[1]}"
-    for _i in range(4):
+
+    # Sentence must be at least 5 tokens long (head plus 3 tails)
+    for _i in range(2):
         tail_options = freq_dict[(head[0], head[1])]
         tail_choice = choices(list(tail_options), [tail_options[tail] for tail in tail_options])
-        while tail_choice[0][-1] in punc_marks:
-            tail_choice = choices(list(tail_options), [tail_options[tail] for tail in tail_options])
+
         sentence += f" {tail_choice[0]}"
         head = (head[1], tail_choice[0])
+
     end_found = False
+
     while not end_found:
         tail_options = freq_dict[(head[0], head[1])]
         tail_choice = choices(list(tail_options), [tail_options[tail] for tail in tail_options])
+
         if tail_choice[0][-1] in punc_marks:
             end_found = True
+
         sentence += f" {tail_choice[0]}"
         head = (head[1], tail_choice[0])
     return sentence
-
 
 
 def main():
@@ -51,5 +60,4 @@ def main():
         print(generate_sentence(freq_dict))
 
 
-nltk.download('all')
 main()
