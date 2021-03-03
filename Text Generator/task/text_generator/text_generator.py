3befa8314c42a9c8d5f519f148e8c8f688f6f4d9
supermads <madisonsfahey@gmail.com>
2021-03-03 14:24:03 -0800

    Stage 6 of Project

--- Text Generator/task/text_generator/text_generator.py
+++ Text Generator/task/text_generator/text_generator.py
@@ -8,10 +8,10 @@ def get_freq_dict():
     with open(filename, "r", encoding="utf-8") as f:
         text = ''.join(f.readlines())
         tokens = text.split()
-        bigrams = list(nltk.bigrams(tokens))
+        trigrams = list(nltk.trigrams(tokens))
         freq_dict = defaultdict(list)
-        for bigram in bigrams:
-            freq_dict[bigram[0]].append(bigram[1])
+        for trigram in trigrams:
+            freq_dict[trigram[0:2]].append(trigram[2])
         for key, value in freq_dict.items():
             freq_dict[key] = Counter(value)
     return freq_dict
@@ -23,25 +23,26 @@ def generate_sentence(freq_dict):
     sentence = ""
     head = choice(list(freq_dict.keys()))
     # First word of the sentence should be capitalized and without punctuation marks
-    while not head[0].isupper() or head[-1] in punc_marks:
+    while not head[0][0].isupper() or head[0][-1] in punc_marks:
         head = choice(list(freq_dict.keys()))
-    sentence += head
+    sentence += f"{head[0]} {head[1]}"
     for _i in range(4):
-        tail_options = freq_dict[head]
+        tail_options = freq_dict[(head[0], head[1])]
         tail_choice = choices(list(tail_options), [tail_options[tail] for tail in tail_options])
         while tail_choice[0][-1] in punc_marks:
             tail_choice = choices(list(tail_options), [tail_options[tail] for tail in tail_options])
         sentence += f" {tail_choice[0]}"
-        head = tail_choice[0]
+        head = (head[1], tail_choice[0])
     end_found = False
     while not end_found:
-        tail_options = freq_dict[head]
+        tail_options = freq_dict[(head[0], head[1])]
         tail_choice = choices(list(tail_options), [tail_options[tail] for tail in tail_options])
         if tail_choice[0][-1] in punc_marks:
             end_found = True
         sentence += f" {tail_choice[0]}"
-        head = tail_choice[0]
+        head = (head[1], tail_choice[0])
     return sentence
+
 
 
 def main():
